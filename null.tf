139cff4d1a580da70e8df2e592ffdc84778004be
Gunjan Khanal <thisisgunjan@yahoo.com>
2023-09-11 22:06:07 +0545

    Update null.tf

--- null.tf
+++ null.tf
@@ -19,3 +19,32 @@ resource "null_resource" "null_resource_simple" {
     command = "echo Hello World"
   }
 }
+
+========================================================================================
+
+#main.tf 
+
+provider "aws" {
+  region     = "eu-central-1"
+  shared_credentials_files = ["/Users/rahulwagh/.aws/credentials"]
+}
+
+resource "aws_instance" "ec2_example" {
+  ami           = "ami-0767046d1677be5a0"
+  instance_type =  "t2.micro"
+  tags = {
+    Name = "Terraform EC2 "
+  }
+}
+
+# The following null resource has the trigger
+resource "null_resource" "null_resource_simple" {
+  
+  # This trigger will only execute once when it detects the instance id of EC2 instance 
+  triggers = {
+    id = aws_instance.ec2_example.id    # to execute it every time replace - id = time()
+  }
+  provisioner "local-exec" {
+    command = "echo Hello World"
+  }
+}
