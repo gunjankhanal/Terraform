8e832f9dfa5620fbf5f39abe4d7bb38c1ef2de56
Gunjan Khanal <thisisgunjan@yahoo.com>
2023-09-11 22:06:51 +0545

    Update null.tf

--- null.tf
+++ null.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "ec2_example" {
 # The following null resource has the trigger
 resource "null_resource" "null_resource_simple" {
   
-  # This trigger will only execute once when it detects the instance id of EC2 instance 
+  # This trigger will only execute once when it detects the instance id of EC2 instance, or some changes seen in the id of ec2_instance
   triggers = {
     id = aws_instance.ec2_example.id    # to execute it every time replace - id = time()
   }
