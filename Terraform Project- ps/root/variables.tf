4af29e07eed4ff6a3fc828319a930470055cdb1e
Gunjan Khanal <thisisgunjan@yahoo.com>
2024-08-25 18:38:04 +0545

    Update variables.tf

--- Terraform Project- ps/root/variables.tf
+++ Terraform Project- ps/root/variables.tf
@@ -1,4 +1,8 @@
-variable region {}
+variable region {
+description = "My description"
+type = string
+default = us-east-1
+}
 variable project_name {}
 variable vpc_cidr {}       
 variable pub_sub_1a_cidr {}
